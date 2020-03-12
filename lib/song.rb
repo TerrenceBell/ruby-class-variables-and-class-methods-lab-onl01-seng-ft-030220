243aad6986797351719e4076517753fe61eaa906
Terrence Bell <terrencekbell@gmail.com>
2020-03-12 22:59:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,12 @@ class Song
 attr_accessor :name, :artist, :genre 
 
 @@count = 0 
-def initialize 
+@@genres = []
+@@artists = []
+def initialize(song_name, artist, genre)
+  @name = song_name 
+  @artist = artist 
+  @genre = genre 
   @@count += 1 
 end 
 def self.count 
